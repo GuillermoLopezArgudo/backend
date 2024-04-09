7aa85c5656108de19ca6320f5c87613545c2eff6
Guillermo <Guillermolopezargudo@gmail.com>
2024-04-09 11:24:10 +0200

    Frontend

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
-
+app.use(express.static('dist'))
 app.use(cors())
 
 morgan.token('body', (request, res) => JSON.stringify(request.body))
